5f24ffe3208f5566b16a273d9cbfc3d00cfac654
Leandro Amaro Garcia <leanamaro@gmail.com>
2022-04-25 14:30:16 -0300

    first commit

---
+++ data/characters/characters.js
@@ -0,0 +1,21 @@
+/*
+Traits Indexes
+0 - Background
+1 - Top
+2 - Hair
+3 - Ears
+4 - Eyes
+5 - Mouth
+6 - Frame
+*/
+
+const characters = [
+  { name: "Toxic", weight: 5, folder: "toxic" },
+  { name: "See thru", weight: 15, folder: "see-throu" },
+  { name: "Scenic", weight: 60, folder: "scenic" },
+  { name: "Solid", weight: 80, folder: "solid" },
+];
+
+module.exports = {
+  characters,
+};
---
+++ data/characters/traits_empty.js
@@ -0,0 +1,63 @@
+/*
+0 - 00_background
+1 - 01_pattern
+2 - 02_body
+3 - 03_crust
+4 - 04_eyes
+5 - 05_mouth
+6 - 06_topper
+7 - 07_Legal Status
+8 - 08_Toast Level
+9 - 09_species
+ */
+
+const folderPath = "01_toxic generation";
+
+const traits = [
+  [
+    //Background - 0
+    
+  ],
+  [
+    //Pattern - 1
+    
+  ],
+  [
+    //Body - 2
+    
+  ],
+  [
+    //Crust - 3
+    
+  ],
+  [
+    //Eyes - 4
+    
+  ],
+  [
+    //Mouth - 5
+    
+  ],
+  [
+    //Topper - 6
+    
+  ],
+  [
+    //Legal Status - 7
+    
+  ],
+  [
+    //Toast Level - 8
+    
+  ],
+  [
+    //Species - 9
+    
+  ],
+ 
+];
+
+module.exports = {
+  traits,
+  folderPath,
+};
